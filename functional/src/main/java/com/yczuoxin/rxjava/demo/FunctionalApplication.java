370bd8142df82d62745f6d0f2a0bba2fa1761883
zuoxin <343720929@qq.com>
2019-03-27 14:27:03 +0800

    spring-boot的web项目框架

---
+++ functional/src/main/java/com/yczuoxin/rxjava/demo/FunctionalApplication.java
@@ -0,0 +1,11 @@
+package com.yczuoxin.rxjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionalApplication.class, args);
+    }
+}
